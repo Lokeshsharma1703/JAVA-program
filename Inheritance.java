4b77a788ab14ffc3eab341b4cb1c503c0e99423d
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-11-23 19:49:21 +0530

    Add files via upload
    Inheritance example

---
+++ Inheritance.java
@@ -0,0 +1,28 @@
+class Emp
+{
+    int Eid;
+    String name;
+    public Emp()
+    {
+        System.out.println("Int Emp...");
+    }
+}
+class perEmp extends Emp
+{
+    int basic_pay;
+    public perEmp()
+    {
+        System.out.println("In PerEmp...");
+    }
+}
+public class Inheritance
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Object of Emp class..");
+        Emp obj = new Emp();
+        System.out.println("------------------------------------------");
+        System.out.println("object of perEmp class..");
+        perEmp obj2 = new perEmp();
+    }
+}
