2c3ba4bec6fe1aac2119760332770abecdceac1f
Lokesh <80037190+Lokeshsharma1703@users.noreply.github.com>
2021-11-30 18:08:52 +0530

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,3 +1,9 @@
+/*Create class Number with only one private instance variable as a int primitive type. Initialize it with parameterised constructor and include the following methods isZero( ),
+isPositive(), isNegative( ), isOdd( ), isEven( ), isPrime(), isAmstrong() the above methods return boolean primitive type value. 
+getFactorial(), getSqrt(), getSqr(), sumDigits()  these methods return int primitive type value.  
+dispBinary(), dispOctal(), displayhexa() these method return string value */
+
+
 import java.lang.Math;
 public class Number
 {
